c3f7ea02aec809bd4cb8622324f8e64a6d33a59c
webmasterradeisan <webmasterradeisan@gmail.com>
2025-08-26 10:37:38 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Routes from "./Routes";
-import "./styles/tailwind.css";
-import "./styles/index.css";
 
 function App() {
   return (
