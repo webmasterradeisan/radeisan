194deb127762eaa1e00b6504978f173042e07932
webmasterradeisan <webmasterradeisan@gmail.com>
2025-08-26 10:43:08 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
 import Routes from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </BrowserRouter>
+    <Routes />
   );
 }
 
