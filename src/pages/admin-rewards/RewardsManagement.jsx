6b37a0f9a95c61f03a111530ca3f393951d4034b
webmasterradeisan <webmasterradeisan@gmail.com>
2025-10-25 23:30:04 -0500

    Update RewardsManagement.jsx

--- src/pages/admin-rewards/RewardsManagement.jsx
+++ src/pages/admin-rewards/RewardsManagement.jsx
@@ -10,7 +10,7 @@
 // ============================================================================
 
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../supabase';
+import { supabase } from '../../lib/supabase';
 import AppIcon from '../../components/AppIcon';
 
 // ============================================================================
