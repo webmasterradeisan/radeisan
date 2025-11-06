402a181e5dc9bc6b468fb69217ac7656cd28a871
webmasterradeisan <webmasterradeisan@gmail.com>
2025-11-06 08:03:29 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import Routes from "./Routes";
+import { AuthProvider } from './contexts/AuthContext';
+import { PointsProvider } from './contexts/PointsContext';
+
 function App() {
   return (
-    <Routes />
+    // 1. AuthProvider debe ser el más externo
+    <AuthProvider>
+      {/* 2. PointsProvider debe ir dentro, ya que usa useAuth() */}
+      <PointsProvider>
+        {/* 3. Tus rutas y toda la aplicación van dentro */}
+        <Routes />
+      </PointsProvider>
+    </AuthProvider>
   );
 }
+
 export default App;
