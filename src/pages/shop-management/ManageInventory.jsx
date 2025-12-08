15899549319181d6f91f4b09d87104e06a0a9ca5
webmasterradeisan <webmasterradeisan@gmail.com>
2025-12-08 17:41:34 -0500

    Create ManageInventory.jsx

---
+++ src/pages/shop-management/ManageInventory.jsx
@@ -0,0 +1,7 @@
+// src/pages/shop-management/ManageInventory.jsx
+// Este componente simplemente reutiliza ShopInventory
+// para la ruta /shop/manage/inventory
+
+import ShopInventory from '../admin-shop/ShopInventory';
+
+export default ShopInventory;
