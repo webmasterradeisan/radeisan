945a96e413575864dd881811178dec5f77ee99dc
webmasterradeisan <webmasterradeisan@gmail.com>
2025-12-08 17:39:51 -0500

    Create ManageOrders.jsx

---
+++ src/pages/shop-management/ManageOrders.jsx
@@ -0,0 +1,7 @@
+// src/pages/shop-management/ManageOrders.jsx
+// Este componente simplemente reutiliza OrdersManagement
+// para la ruta /shop/manage/orders
+
+import OrdersManagement from '../admin-shop/OrdersManagement';
+
+export default OrdersManagement;
